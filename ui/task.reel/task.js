37906dfcab743cd6f966a02155ec5e92df58f7c1
Dean Holt <noreply>
2017-08-14 03:23:59 +0000

    Add component task

---
+++ ui/task.reel/task.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/task.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Task
+ * @extends Component
+ */
+exports.Task = Component.specialize(/** @lends Task# */ {
+    constructor: {
+        value: function Task() {
+            this.super();
+        }
+    }
+});
